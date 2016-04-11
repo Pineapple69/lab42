a9ed57386c0db8f231bb7e451ce2261649d0c040
Roman Kucherenko <Roman Kucherenko>
2016-04-11 16:40:58 +0300

    With new changePrice method

--- JavaApplication12/src/javaapplication12/JavaApplication12.java
+++ JavaApplication12/src/javaapplication12/JavaApplication12.java
@@ -10,6 +10,7 @@ import static javaapplication12.JavaApplication12.changeBalance;
 /**
  *
  * @author sofastarikova
+ * @author RomanKucherenko
  */
  
 public class JavaApplication12 {
@@ -17,30 +18,36 @@ public class JavaApplication12 {
          {
              int newvalue = 10;
              return newvalue;
-         }
+         }  
+    public static int changePrice() {
+            int newvalue = 20;
+            return newvalue;
+        }	 
  public static void main(String[] args) {
- Product product = new Product("Edam", 3.3, 120);
- System.out.println("Product value is " + product.countValue());
- product.printProduct();
- JavaApplication12.changeBalance();
+	Product product = new Product("Edam", 3.3, 120);
+	System.out.println("Product value is " + product.countValue());
+	product.printProduct();
+	JavaApplication12.changeBalance();
+	JavaApplication12.changePrice();
  }
  
 }
 class Product {
-private String name;
-private double price;
-private int amount; //Amount in storage
-public Product(String name, double price, int amount) {
-this.name = name;
-this.price = price;
-this.amount = amount;
+	private String name;
+	private double price;
+	private int amount; //Amount in storage
+	public Product(String name, double price, int amount) {
+	this.name = name;
+	this.price = price;
+	this.amount = amount;
 }
 public double countValue() {
-return(amount * price);
+	return(amount * price);
 }
 public void printProduct() {
-System.out.printf("Product %s, price %4.1f and balance %d pcs",
-name,price,amount);
+	System.out.printf("Product %s, price %4.1f and balance %d pcs",
+	name,price,amount);
 }
+
 }
     
